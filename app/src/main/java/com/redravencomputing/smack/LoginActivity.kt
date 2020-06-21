4c40404225a2344ccb3b75d20c621e9b59cc4684
Bruce <bruce@redravencomputing.com>
2020-06-21 12:28:09 -0600

    Add a create user screen

--- app/src/main/java/com/redravencomputing/smack/LoginActivity.kt
+++ app/src/main/java/com/redravencomputing/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.redravencomputing.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,7 +12,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view: View) {
-
+        val intent = Intent(this, CreateUserActivity::class.java)
+        startActivity(intent)
     }
 
     fun loginLoginBtnClicked(view: View) {
