ddac7c1c4953cb4c673fb460854b72e640469500
Bruce <bruce@redravencomputing.com>
2020-07-16 16:03:35 -0600

    Add ability to login

--- app/src/main/java/com/redravencomputing/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/redravencomputing/smack/Controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.redravencomputing.smack.R
+import com.redravencomputing.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +21,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
 
+        AuthService.loginUser(this, email, password) { loginSuccess ->
+            if (loginSuccess) {
+                AuthService.findUserByEmail(this) { findSuccess ->
+                    if (findSuccess) {
+                        finish()
+                    }
+                }
+            }
+        }
     }
 
 }
